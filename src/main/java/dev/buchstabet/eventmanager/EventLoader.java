a2db93c13418c98331a9a0f40e3c5701afcb4390
Buchstabet <konstantinkrotz@gmail.com>
2023-03-22 17:40:59 +0100

    work

--- src/main/java/dev/buchstabet/eventmanager/EventLoader.java
+++ src/main/java/dev/buchstabet/eventmanager/EventLoader.java
@@ -2,12 +2,14 @@ package dev.buchstabet.eventmanager;
 
 
 import lombok.Getter;
+import org.w3c.dom.events.DocumentEvent;
 import org.w3c.dom.events.EventException;
 
 import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -24,7 +26,10 @@ public class EventLoader {
     this(new ArrayList<>());
   }
 
-  public <V extends Event> void call(V v) {
+  /**
+   * @param v The event that is to be triggered.
+   */
+  public <V extends Event> void throwEvent(V v) {
     eventClasses.stream().filter(eventMethod -> eventMethod.getEvent().isInstance(v)).forEach(eventMethod -> {
       try {
         eventMethod.getMethod().invoke(eventMethod.getInstance(), v);
@@ -34,15 +39,19 @@ public class EventLoader {
     });
   }
 
+
   public void register(Object o) throws EventException {
-    List<EventMethod> collect = Arrays.stream(o.getClass().getDeclaredMethods()).
+    register(o, o.getClass());
+  }
+  public void register(Object o, Class<?> clazz) throws EventException {
+    List<EventMethod> collect = Arrays.stream(clazz.getDeclaredMethods()).
             filter(method -> method.getParameterTypes().length == 1).
             filter(method -> Arrays.asList(method.getParameterTypes()[0].getInterfaces()).contains(Event.class)).
+            filter(method -> method.isAnnotationPresent(EventManager.class)).
             map(method -> new EventMethod(method, (Class<? extends Event>) method.getParameterTypes()[0], o)).
             collect(Collectors.toList());
     eventClasses.addAll(collect);
   }
-
 
 
 }
